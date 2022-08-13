62207d288ba4aa474bf058f0556648c294ec41da
giseleseol <101464400+giseleseol@users.noreply.github.com>
2022-08-13 17:26:06 -0300

    Add files via upload

--- PRO-V2-C27-Modelo-main/PRO-V2-C27-Modelo-main/js/PlayerArrow.js
+++ PRO-V2-C27-Modelo-main/PRO-V2-C27-Modelo-main/js/PlayerArrow.js
@@ -51,20 +51,20 @@ class PlayerArrow {
     image(this.image, 0, 0, this.width, this.height);
     pop();
 
-    /*if (this.body.velocity.x > 0 && this.body.position.x > 400) {
+    if (this.body.velocity.x > 0 && this.body.position.x > 400) {
       var position = [this.body.position.x, this.body.position.y];
       this.trajectory.push(position);
-    }*/
+    }
     
     /*if (this.body.velocity.x > 0 || this.body.position.x > 400) {
       var position = [this.body.position.x, this.body.position.y];
       trajectory.push(position);
     }*/
 
-    if (this.body.velocity.x < 0 && this.body.position.x < 400) {
+    /*if (this.body.velocity.x < 0 && this.body.position.x < 400) {
       var position = [this.body.position.x, this.body.position.y];
       this.trajectory(position);
-    }
+    }*/
 
     /*if (this.body.velocity.x > 0 this.body.position.x > 400) {
       var position = [this.body.position.x, this.body.position.y];
